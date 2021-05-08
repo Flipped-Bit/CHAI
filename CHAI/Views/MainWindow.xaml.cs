dc596048a8e5ccd253beadb0c7c9f2126b6ac01c
Flipped-Bit <Flipped_Bit@outlook.com>
2021-05-08 22:01:01 +0100

    Updated constructor to populate list of Triggers

--- CHAI/Views/MainWindow.xaml.cs
+++ CHAI/Views/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CHAI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using Trigger = CHAI.Models.Trigger;
 
 namespace CHAI.Views
 {
@@ -9,6 +14,11 @@ namespace CHAI.Views
     public partial class MainWindow : Window
     {
         /// <summary>
+        /// The injected <see cref="CHAIDbContext"/>.
+        /// </summary>
+        private readonly CHAIDbContext _context;
+
+        /// <summary>
         /// The injected <see cref="ILogger"/>.
         /// </summary>
         private readonly ILogger _logger;
@@ -16,11 +26,21 @@ namespace CHAI.Views
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
         /// </summary>
-        public MainWindow(ILogger<MainWindow> logger)
+        public MainWindow(CHAIDbContext context, ILogger<MainWindow> logger)
         {
+            _context = context;
             _logger = logger;
             InitializeComponent();
+            UpdateTriggersList();
             _logger.LogInformation("Main window initialised successfully");
+        }
+
+        /// <summary>
+        /// Method for updating the <see cref="TriggersList"/> with all <see cref="Trigger"/>s.
+        /// </summary>
+        private void UpdateTriggersList()
+        {
+            TriggersList.ItemsSource = _context.Triggers.ToList();
         }
     }
 }
