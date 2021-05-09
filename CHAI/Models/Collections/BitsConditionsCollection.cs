9c1716fd75f10d15dc4f536f8f77b67521774e07
Flipped-Bit <Flipped_Bit@outlook.com>
2021-05-09 23:25:45 +0100

    Added collections for both Enum types

---
+++ CHAI/Models/Collections/BitsConditionsCollection.cs
@@ -0,0 +1,25 @@
+﻿using CHAI.Extensions;
+using CHAI.Models.Enums;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CHAI.Models.Collections
+{
+    /// <summary>
+    /// An <see cref="ObservableCollection{BitsCondition}"/>.
+    /// </summary>
+    public class BitsConditionsCollection : ObservableCollection<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BitsConditionsCollection"/> class.
+        /// </summary>
+        public BitsConditionsCollection()
+        {
+            foreach (var bitCondition in Enum.GetValues(typeof(BitsCondition)).Cast<BitsCondition>())
+            {
+                Add(bitCondition.GetDescription());
+            }
+        }
+    }
+}
---
+++ CHAI/Models/Collections/CooldownUnitsCollection.cs
@@ -0,0 +1,25 @@
+﻿using CHAI.Extensions;
+using CHAI.Models.Enums;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CHAI.Models.Collections
+{
+    /// <summary>
+    /// An <see cref="ObservableCollection{CooldownUnit}"/>.
+    /// </summary>
+    public class CooldownUnitsCollection : ObservableCollection<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CooldownUnitsCollection"/> class.
+        /// </summary>
+        public CooldownUnitsCollection()
+        {
+            foreach (var cooldownUnit in Enum.GetValues(typeof(CooldownUnit)).Cast<CooldownUnit>())
+            {
+                Add(cooldownUnit.GetDescription());
+            }
+        }
+    }
+}
