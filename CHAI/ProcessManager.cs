39ba523a0f7e02dd74ef46d4d88a2dddceec75aa
Flipped-Bit <Flipped_Bit@outlook.com>
2021-05-14 22:40:20 +0100

    Added process manager

---
+++ CHAI/ProcessManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace CHAI
+{
+    /// <summary>
+    /// Manager for Interacting with <see cref="Process"/>es.
+    /// </summary>
+    public static class ProcessManager
+    {
+        /// <summary>
+        /// Method for checking if seleceted <see cref="Process"/> is running.
+        /// </summary>
+        /// <param name="processName"><see cref="Process"/> to check.</param>
+        /// <returns>A boolean value indicating whether <see cref="Process"/> is running.</returns>
+        public static bool ProcessRunning(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            return processes.Length > 0;
+        }
+    }
+}
