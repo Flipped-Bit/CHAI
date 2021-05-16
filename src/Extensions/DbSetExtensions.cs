026fb94f6d48936055b4857a0bd61ab0c52f7e7e
Flipped-Bit <Flipped_Bit@outlook.com>
2021-05-16 14:56:25 +0100

    Added extension for clearing database tables

---
+++ src/Extensions/DbSetExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CHAI.Extensions
+{
+    /// <summary>
+    /// Entensions for <see cref="DbSet{TEntity}"/>.
+    /// </summary>
+    public static class DbSetExtensions
+    {
+        /// <summary>
+        /// Method for removing all data in a <see cref="DbSet{TEntity}"/>.
+        /// </summary>
+        /// <typeparam name="T">The TEntity of the DbSet.</typeparam>
+        /// <param name="dbSet">The DbSet to remove data from.</param>
+        public static void Clear<T>(this DbSet<T> dbSet)
+            where T : class
+        {
+            dbSet.RemoveRange(dbSet);
+        }
+    }
+}
